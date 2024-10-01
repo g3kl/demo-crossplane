12ea5ab1ac7be906b5ac8a1919dd8b2c6726972a
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-10-01 10:52:18 +1000

    Create main.tf

---
+++ config_as_code/main.tf
@@ -0,0 +1,25 @@
+resource "dynatrace_autotag_v2" "crossplane-created" {
+  name                          = "crossplane-created"
+  description                   = "this is an autotag rule created by crossplane"
+  # rules_maintained_externally = false
+  rules {
+    rule {
+      type                = "ME"
+      enabled             = true
+      value_normalization = "Leave text as-is"
+      attribute_rule {
+        entity_type                 = "SERVICE"
+        service_to_host_propagation = true
+        service_to_pgpropagation    = true
+        conditions {
+          condition {
+            case_sensitive = true
+            key            = "HOST_GROUP_NAME"
+            operator       = "EQUALS"
+            string_value   = "crossplane"
+          }
+        }
+      }
+    }
+  }
+}
